48f9b9c1a41aaf40064bef7c8a405aeffbe97dad
ssamarin <semenssamarin@mail.ru>
2023-02-25 18:19:06 +1000

    feature: delete employees onClickbtn-trash button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,43 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: "John C.", salary: 800, increase: false, id: 1},
+                {name: "Alex M.", salary: 3000, increase: true, id: 2},
+                {name: "Carl W.", salary: 15000, increase: false, id: 3},
+            ]
+        }
+    }
 
-    const data = [
-        {name: "John C.", salary: 800, increase: false, id: 1},
-        {name: "Alex M.", salary: 3000, increase: true, id: 2},
-        {name: "Carl W.", salary: 15000, increase: false, id: 3},
-    ];
-
-    return (
-        <div className="app">
-            <AppInfo/>
-
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        });
+    }
+ 
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
